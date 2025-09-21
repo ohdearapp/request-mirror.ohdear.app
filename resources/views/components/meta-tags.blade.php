3d0fc3e756f51213bc4388104ca52d1bcab189d4
Freek Van der Herten <freek@spatie.be>
2025-09-22 00:04:31 +0200

    wip

--- resources/views/components/meta-tags.blade.php
+++ resources/views/components/meta-tags.blade.php
@@ -7,7 +7,7 @@
 <meta property="og:type" content="website">
 <meta property="og:title" content="{{ $title ?? 'Request Mirror - HTTP Testing Service by Oh Dear' }}">
 <meta property="og:description" content="The simple HTTP request testing tool. Send a request and get instant JSON response with all request details. Perfect for API development, webhook testing, and learning HTTP. Free service by Oh Dear.">
-<meta property="og:image" content="{{ asset('img/og-image.jpg') }}">
+<meta property="og:image" content="{{ asset('img/og-image.png') }}">
 
 <!-- Twitter Meta Tags -->
 <meta name="twitter:card" content="summary_large_image">
@@ -15,4 +15,4 @@
 <meta property="twitter:url" content="{{ url()->current() }}">
 <meta name="twitter:title" content="{{ $title ?? 'Request Mirror - HTTP Testing Service by Oh Dear' }}">
 <meta name="twitter:description" content="The simple HTTP request testing tool. Send a request and get instant JSON response with all request details. Perfect for API development, webhook testing, and learning HTTP. Free service by Oh Dear.">
-<meta name="twitter:image" content="{{ asset('img/og-image.jpg') }}">
+<meta name="twitter:image" content="{{ asset('img/og-image.png') }}">
