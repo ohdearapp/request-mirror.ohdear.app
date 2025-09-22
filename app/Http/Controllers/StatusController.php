1aa655ba9af6b9b3b680cc68f209833eeeb9eb25
Freek Van der Herten <freek@spatie.be>
2025-09-22 09:41:55 +0200

    wip

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,39 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StatusCodeRequest;
 use App\Traits\RequestMirrorTrait;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class StatusController
 {
     use RequestMirrorTrait;
 
-    public function __invoke(Request $request, string $codes): JsonResponse
+    public function __invoke(StatusCodeRequest $request): JsonResponse
     {
-        $statusCode = $this->parseStatusCode($codes);
+        $statusCode = $request->getParsedStatusCode();
 
         $response = $this->buildResponse($request, [
             'status' => $statusCode,
         ]);
 
         return $response->setStatusCode($statusCode);
-    }
-
-    private function parseStatusCode(string $codes): int
-    {
-        $codeList = array_filter(explode(',', $codes), function ($code) {
-            return is_numeric(trim($code)) && trim($code) >= 100 && trim($code) <= 599;
-        });
-
-        if (empty($codeList)) {
-            return 200;
-        }
-
-        if (count($codeList) === 1) {
-            return (int) trim($codeList[0]);
-        }
-
-        return (int) trim($codeList[array_rand($codeList)]);
     }
 }
