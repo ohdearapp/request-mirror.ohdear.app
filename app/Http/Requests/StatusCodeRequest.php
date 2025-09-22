3ac1e9993aae1463c77935aeb6147278e92fe85d
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-09-22 07:42:44 +0000

    fix code style

--- app/Http/Requests/StatusCodeRequest.php
+++ app/Http/Requests/StatusCodeRequest.php
@@ -9,9 +9,9 @@ class StatusCodeRequest extends FormRequest
     public function getParsedStatusCode(): int
     {
         $codes = collect(explode(',', $this->route('codes')))
-            ->map(fn($code) => trim($code))
-            ->filter(fn($code) => is_numeric($code) && $code >= 100 && $code <= 599)
-            ->map(fn($code) => (int) $code)
+            ->map(fn ($code) => trim($code))
+            ->filter(fn ($code) => is_numeric($code) && $code >= 100 && $code <= 599)
+            ->map(fn ($code) => (int) $code)
             ->values();
 
         return $codes->isEmpty() ? 200 : $codes->random();
