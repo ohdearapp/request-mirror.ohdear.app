ee3fdb460bfc4573f809efb25283d408a2fd368c
Mattias Geniar <m@ttias.be>
2025-10-01 13:20:48 +0200

    fix test

--- tests/Feature/RobotsEndpointTest.php
+++ tests/Feature/RobotsEndpointTest.php
@@ -1,11 +1,11 @@
 <?php
 
 it('returns robots.txt content', function () {
-    $response = $this->get('/robots.txt');
+    $response = $this->get('/robots-txt');
 
     $response->assertOk()
         ->assertHeader('Content-Type', 'text/plain; charset=UTF-8');
 
     expect($response->content())
-        ->toContain('User-agent: *');
+        ->toContain('Sitemap');
 });
