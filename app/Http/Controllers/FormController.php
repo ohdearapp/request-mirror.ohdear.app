b205405fa32e43cbfe5f3d0b9a0cf8552ce9bbc7
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-10-08 16:24:08 +0000

    fix code style

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class FormController
 {
