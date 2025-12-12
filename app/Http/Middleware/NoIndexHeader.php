06265c08f33235a2fbb127954ba0fd91051d9e80
Mattias Geniar <m@ttias.be>
2025-12-12 12:03:07 +0100

    wip

--- app/Http/Middleware/NoIndexHeader.php
+++ app/Http/Middleware/NoIndexHeader.php
@@ -11,7 +11,9 @@ class NoIndexHeader
     /**
      * Handle an incoming request.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
